fcf0f51346d7d7dd7b7be79b026e0e7f88e1dede
azizulwd <azizulislamgd@gmail.com>
2021-11-04 05:01:17 +0600

    testimonial added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -4,6 +4,7 @@ import Appointment from '../Appointment/Appointment';
 import Banner from '../Banner/Banner';
 import Facility from '../Facility/Facility';
 import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <Facility></Facility>
             <Appointment></Appointment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
---
+++ src/components/Home/Testimonial/Testimonial.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Testimonial.css';
+import people1 from '../../../images/people-1.png';
+import people2 from '../../../images/people-2.png';
+import people3 from '../../../images/people-3.png';
+
+const Testimonial = () => {
+    return (
+        <div className="testimonial">
+            <div className="testimonial_title">
+                <h3>TESTIMONIAL</h3>
+                <h1>What's Our Patients Says</h1>
+            </div>
+            <div className="container testimonial_inner">
+                <div className="testimonial_user">
+                    <div className="testimonial_text">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti dicta eum ullam reprehenderit inventore dolorem! Quo et dignissimos corrupti provident?</p>
+                    </div>
+                    <div className="testinial_user_info">
+                        <div className="testinial_user_pic">
+                            <img src={people1} alt="" />
+                        </div>
+                        <div className="testinial_user_details">
+                            <h4>Windson Herry</h4>
+                            <p>California</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="testimonial_user">
+                    <div className="testimonial_text">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti dicta eum ullam reprehenderit inventore dolorem! Quo et dignissimos corrupti provident?</p>
+                    </div>
+                    <div className="testinial_user_info">
+                        <div className="testinial_user_pic">
+                            <img src={people2} alt="" />
+                        </div>
+                        <div className="testinial_user_details">
+                            <h4>Windson Herry</h4>
+                            <p>California</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="testimonial_user">
+                    <div className="testimonial_text">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti dicta eum ullam reprehenderit inventore dolorem! Quo et dignissimos corrupti provident?</p>
+                    </div>
+                    <div className="testinial_user_info">
+                        <div className="testinial_user_pic">
+                            <img src={people3} alt="" />
+                        </div>
+                        <div className="testinial_user_details">
+                            <h4>Windson Herry</h4>
+                            <p>California</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonial;
