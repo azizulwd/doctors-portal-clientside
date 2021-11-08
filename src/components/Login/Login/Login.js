b28e69acb61aee2bbe13470cb44654c63a85a1e6
azizulwd <azizulislamgd@gmail.com>
2021-11-08 23:36:39 +0600

    login functionality added

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import loginImg from '../../../images/login.png';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({});
+
+    const handleOnChange = e =>{
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = {...loginData};
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
 
     const handleLoginForm = e =>{
         e.preventDefault();
@@ -17,8 +26,8 @@ const Login = () => {
                         <h3>Login</h3>
                     </div>
                     <form onSubmit={handleLoginForm}>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
+                        <input onChange={handleOnChange} type="email" placeholder="Email" name="email" />
+                        <input onChange={handleOnChange} type="password" placeholder="Password" name="password" />
                         <button type="submit">Login</button>
                     </form>
                     <div className="toggle_form">
