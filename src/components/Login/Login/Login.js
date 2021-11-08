d6967ac4b939ce3045ebef3f8940e226afa8e908
azizulwd <azizulislamgd@gmail.com>
2021-11-08 21:41:55 +0600

    login form added

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import loginImg from '../../../images/login.png';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
+
+    const handleForm = e =>{
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <div className="container login_inner">
+                <div className="login_form">
+                    <div className="login_header">
+                        <h3>Login</h3>
+                    </div>
+                    <form onSubmit={handleForm}>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit">Login</button>
+                    </form>
+                    <div className="toggle_form">
+                        <Link to="/register">New user? Create an Account.</Link>
+                    </div>
+                </div>
+                <div className="login_img">
+                    <img src={loginImg} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
